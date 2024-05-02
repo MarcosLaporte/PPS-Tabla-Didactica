9fa68f4fb4704686aa6046140c21689aab37d509
LapMarcos <marcoslaporte2015@gmail.com>
2024-05-02 11:43:50 -0300

    Base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    NgxSpinnerModule,
+    provideFirebaseApp(() => initializeApp({
+      "projectId": "primer-parcial-app1",
+      "appId": "1:915380151213:web:e25ef4b9543b87809e7724",
+      "storageBucket": "primer-parcial-app1.appspot.com",
+      "apiKey": "AIzaSyBiLy7FGCZADs_CuYXVrsnUUvmT8sWfXtc",
+      "authDomain": "primer-parcial-app1.firebaseapp.com",
+      "messagingSenderId": "915380151213"
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule {}
+export class AppModule { }
